0006c325c32bdc0fa9bf3edd75a64b183a710651
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-08-12 23:08:54 +0530

    created ChatScreen, added ChatScreen routeName to the routes table
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/chat_screen.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const ChatScreen(),
     );
   }
 }
